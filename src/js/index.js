d5acd21c7579905175b48cf06f3706de37f5b390
hamza abushamas <hamzah960@gmail.com>
2022-07-11 19:14:55 +0300

    finish main page

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,17 @@
-import '../sass/style.scss';
 import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min.js';
+import '../sass/style.scss';
+import '@fortawesome/fontawesome-free/js/all.min.js';
+
+$(document).ready(function(){
+    $('.thumbnail2').hover(function(){
+        
+        $(this).find(".project-category").toggle();
+        $(this).find(".caption2").slideToggle(300);
+    });
 
 
-console.log("hello webpack");
+    $("#date").text(new Date().getFullYear());
+});
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     entry: './src/js/index.js',
 
     output: {
+        publicPath: '/',
         path: path.resolve(__dirname, 'build'),
         filename: 'js/bundle.js'
     },
@@ -18,41 +19,79 @@ module.exports = {
             {
                 test: /\.(sass|css|scss)$/,
                 use: [
-                  // Creates `style` nodes from JS strings
-                  {
-                    loader: MiniCssExtractPlugin.loader, 
-                    options: {
-                      publicPath: '../'
-                    }
-                  },
-                  // Translates CSS into CommonJS
-                  "css-loader",
-                  // Compiles Sass to CSS
-                  "sass-loader",
+                    // Creates `style` nodes from JS strings
+                    {
+                        loader: MiniCssExtractPlugin.loader, 
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
                 ],
             },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: "images",
+                        }
+                    },
+                ],
+            },
+
+            {
+                test: /\.(svg|eot|woof|woof2|ttf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: "fonts",
+                        }
+                    },
+                ],
+            },
+
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            
+            {
+                test: require.resolve("jquery"),
+                loader: "expose-loader",
+                options: {
+                    exposes: ["$", "jquery"],
+                },
+            },
+            
         ],
     },
 
     devServer: {
-        static: {
-            directory: path.join(__dirname, 'build'),
-        },
+        contentBase: path.join(__dirname, "build"),
         // compress: true,
         port: 9000,
+        writeToDisk: true,
+        stats: "errors-only",
         open: true,
-        devMiddleware: {
-            writeToDisk: true,
-            stats: 'errors-only'
-        }
     },
+    
 
     plugins: [
         new HtmlWebpackPlugin({
         template: './src/index.html',
         filename: 'index.html'
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "css/style.css",
+        }),
         // new CleanWebpackPlugin()
     ],
 };
